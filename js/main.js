867d1e29c978fc47ae22613723037d63f1ab3cc3
SaltPepperSalt <dksro324@gmail.com>
2020-04-04 18:34:39 +0900

    project done

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// mini project js
+var menuOpen = $('.btnMenuopen');
+var menuClose = $('.btnMenuClose');
+var infoOpen = $('.infoButton');
+var infoClose = $('.closeinfoButton');
+var infoCard = $('.infoCard')
+
+menuOpen.on('click keyup', function (e) {
+    e.preventDefault();
+    if (e.keyCode === 13 || e.type === 'click') {
+        $(this).parent().addClass('isAct');
+    }
+});
+
+menuClose.on('click keyup', function (e) {
+    e.preventDefault();
+    if (e.keyCode === 13 || e.type === 'click') {
+        $(this).parent().removeClass('isAct');
+    }
+});
+
+infoOpen.on('click keyup', function (e) {
+    e.preventDefault();
+    if (e.keyCode === 13 || e.type === 'click') {
+        $(this).siblings(infoCard).css('transition','opacity .5s linear');
+        $(this).siblings(infoCard).addClass('infoOpen');
+    }
+});
+
+infoClose.on('click keyup', function (e) {
+    e.preventDefault();
+    if (e.keyCode === 13 || e.type === 'click') {
+        $(this).parent().css('transition','opacity .5s linear, z-index 0s .5s')
+        $(this).parent().removeClass('infoOpen');
+    }
+});
